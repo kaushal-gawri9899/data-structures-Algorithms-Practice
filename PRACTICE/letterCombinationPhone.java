29f34eaba67511c54609f9c335a4d78eb9175eec
kaushal-gawri9899 <gawrikaushal9899@gmail.com>
2021-02-19 12:32:54 +0530

    Revisiting LeetCode Problems. Added in "PRACTICE"

---
+++ PRACTICE/letterCombinationPhone.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    List<String> solution = new ArrayList();
+    HashMap<String,String> map = new HashMap<>();
+
+    public List<String> letterCombinations(String digits)
+    {
+
+        map.put("2","abc");
+        map.put("3","def");
+        map.put("4","ghi");
+        map.put("5","jkl");
+        map.put("6","mno");
+        map.put("7","pqrs");
+        map.put("8","tuv");
+        map.put("9","wxyz");
+
+
+        letterCombinationsHelper("",digits);
+        return solution;
+
+    }
+
+    //Input : "234"
+
+    private void letterCombinationsHelper(String value, String currDigits)
+    {
+        if(currDigits.length()!=0)
+        {
+            String currDigit = currDigits.substring(0,1);
+            String letter = map.get(currDigit);
+
+            for(int i=0; i<letter.length(); i++)
+            {
+                String word = map.get(currDigit).substring(i,i+1);
+
+               // currDigits.substring specifies next substring from Input
+                letterCombinationsHelper(value+word, currDigits.substring(1));
+            }
+        }
+        else{
+            solution.add(value);
+        }
+    }
+}
